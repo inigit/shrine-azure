01e398b0f753630987aa4d738b030f3e2445f63a
Huynh Truong <huynh.truong@bstarsolutions.com>
2018-09-26 16:31:45 +0700

    Fixed syntax

--- lib/shrine/azure.rb
+++ lib/shrine/azure.rb
@@ -26,7 +26,7 @@ class Shrine
             file = io.tempfile
           end
           Rails.logger.info("[File]: #{file.inspect}")
-          options = { :content_type => io.content_type,  content_disposition: 'attachment; filename=' + io.original_filenames }
+          options = { :content_type => io.content_type,  content_disposition: 'attachment; filename=' + io.original_filename }
           blobs.create_block_blob("#{container}#{prefix}", id, file, options)
         rescue Azure::Core::Http::HTTPError
           raise Shrine::Error
